6a9fca54ca42bda3a84c79937469bd3d2784c019
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-10-28 14:20:05 +0100

    Chat model not unloaded when the user change the IA tools tab

--- src/js/react-chat.js
+++ src/js/react-chat.js
@@ -4,119 +4,152 @@ import { ChatWebLLM } from "@langchain/community/chat_models/webllm";
 import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { AIMessage, HumanMessage } from '@langchain/core/messages';
 
+
+let g_chatData = null;
+
 export function useChatModel({ promptMessage, model, baseUrl = "webllm" }) {
-  const [chain, setChain] = useState(null);
-  const [progress, setProgress] = useState("");
-  const [chat, setChat] = useState(null);
+    const [chain, setChain] = useState(null);
+    const [progress, setProgress] = useState("");
+    const [chat, setChat] = useState(null);
 
-  useEffect(() => {
-    if (chat) {
-      const template = ChatPromptTemplate.fromMessages([
-        ["system", promptMessage],
-        new MessagesPlaceholder("chat_history"),
-        ["user", "Pregunta: {input}"]
-      ]);
-  
-      setChain(template.pipe(chat));
-    }
-  }, [chat, promptMessage]);
+    useEffect(() => {
+        if (chat) {
+            const template = ChatPromptTemplate.fromMessages([
+                ["system", promptMessage],
+                new MessagesPlaceholder("chat_history"),
+                ["user", "Pregunta: {input}"]
+            ]);
 
-  useEffect(() => {
-    setChain(null);
-    setChat(null);
-    
-
-    if (baseUrl !== "webllm") {
-      const chatOllama = new ChatOllama({
-        model,
-        baseUrl,
-        temperature: 0.1
-      });
-      setChat(chatOllama);
-    }
-    else {
-      console.log("Reloading model");
-      const webllmChat = new ChatWebLLM({
-        model,
-        chatOptions: {
-          temperature: 0.1
+            setChain(template.pipe(chat));
         }
-      });
-  
-      webllmChat.initialize((progress) => {
-        setProgress(progress.text);
-      }).then(() => {
-        setChat(webllmChat);
-      });
-    }
-  }, [promptMessage, model, baseUrl]);
+    }, [chat, promptMessage]);
 
-  return { chat: chain, progress };
+    useEffect(() => {
+        setChain(null);
+        setChat(null);
+
+        if (!g_chatData || g_chatData.model !== model || g_chatData.baseUrl !== baseUrl) {
+            if (baseUrl !== "webllm") {
+                const chatOllama = new ChatOllama({
+                    model,
+                    baseUrl,
+                    temperature: 0.1
+                });
+                setChat(chatOllama);
+                g_chatData = {
+                    chat: chatOllama,
+                    model,
+                    baseUrl
+                }
+            }
+            else {
+                console.log("Reloading model");
+                const webllmChat = new ChatWebLLM({
+                    model,
+                    chatOptions: {
+                        temperature: 0.1
+                    }
+                });
+
+                g_chatData = {
+                    chat: webllmChat,
+                    model,
+                    baseUrl,
+                    progressCallback: (progress) => {
+                        setProgress(progress.text);    
+                    }
+                }
+
+                g_chatData.initializePromise = webllmChat.initialize(p => g_chatData.progressCallback(p));
+
+                g_chatData.initializePromise.then(() => {
+                    setChat(webllmChat);
+                });
+            }
+        }
+        else {
+            if (g_chatData.initializePromise) {
+                g_chatData.progressCallback = (progress) => {
+                    setProgress(progress.text);    
+                }
+                g_chatData.initializePromise.then(() => {
+                    setChat(g_chatData.chat);
+                    g_chatData.initializePromise = null;
+                });
+            }
+            else {
+                setChat(g_chatData.chat);
+            }
+        }
+        
+    }, [model, baseUrl]);
+
+    return { chat: chain, progress };
 }
 
 export function useChat({ promptMessage, model, baseUrl = "webllm", chatHistoryMaxMessages = 5 }) {
-  const [input, setInput] = useState('');
-  const [processing, setProcessing] = useState(false);
-  const [messages, setMessages] = useState([]);
-  const [chatHistory, setChatHistory] = useState([]);
-  const lastMessage = useRef('');
-  const [chain, setChain] = useState(null);
+    const [input, setInput] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [chatHistory, setChatHistory] = useState([]);
+    const lastMessage = useRef('');
+    const [chain, setChain] = useState(null);
 
-  useEffect(() => {
-    console.log("Reset messages");
-    setMessages([]);
-    setChatHistory([]);
-    setChain(null);
-  }, [promptMessage, model, baseUrl]);
+    useEffect(() => {
+        console.log("Reset messages");
+        setMessages([]);
+        setChatHistory([]);
+        setChain(null);
+    }, [promptMessage, model, baseUrl]);
 
-  const { chat, progress : progressMessage } = useChatModel({
-    promptMessage,
-    model,
-    baseUrl
-  });
-
-  useEffect(() => {
-    if (chat) {
-      console.log("Recreating chain");
-      setChain(chat);
-    }
-  }, [chat]);
-
-  useEffect(() => {
-    lastMessage.current = '';
-  }, [chatHistory]);
-
-  const submitMessage = async (e) => {
-    e.preventDefault();
-    setChatHistory(h => [...h.slice(-chatHistoryMaxMessages), new HumanMessage(input)]);
-    setProcessing(true);
-    setMessages(m => [
-        ...(m.slice(-5)),
-        { role: "human", text: input }, { role: "system", "text": "" }
-    ]);
-    setInput(() => '');
-
-    new Promise(resolve => {
-      chain.stream({ input, chat_history: chatHistory })
-        .then(async chunks => {
-          for await (const chunk of chunks) {
-            setMessages(msg => [...msg.slice(0, -1), { ...msg[msg.length - 1], text: msg[msg.length - 1].text + chunk?.content }]);
-            lastMessage.current += chunk?.content;
-          }
-          setChatHistory(h => [...h, new AIMessage(lastMessage.current)]);
-          resolve();
-        })
-    }).then(() => {
-      setProcessing(false);
+    const { chat, progress: progressMessage } = useChatModel({
+        promptMessage,
+        model,
+        baseUrl
     });
-  }
 
-  return {
-    input, setInput,
-    messages,
-    submitMessage,
-    processing,
-    ready: chain !== null,
-    progressMessage
-  }
+    useEffect(() => {
+        if (chat) {
+            console.log("Recreating chain");
+            setChain(chat);
+        }
+    }, [chat]);
+
+    useEffect(() => {
+        lastMessage.current = '';
+    }, [chatHistory]);
+
+    const submitMessage = async (e) => {
+        e.preventDefault();
+        setChatHistory(h => [...h.slice(-chatHistoryMaxMessages), new HumanMessage(input)]);
+        setProcessing(true);
+        setMessages(m => [
+            ...(m.slice(-5)),
+            { role: "human", text: input }, { role: "system", "text": "" }
+        ]);
+        setInput(() => '');
+
+        new Promise(resolve => {
+            chain.stream({ input, chat_history: chatHistory })
+                .then(async chunks => {
+                    for await (const chunk of chunks) {
+                        setMessages(msg => [...msg.slice(0, -1), { ...msg[msg.length - 1], text: msg[msg.length - 1].text + chunk?.content }]);
+                        lastMessage.current += chunk?.content;
+                    }
+                    setChatHistory(h => [...h, new AIMessage(lastMessage.current)]);
+                    resolve();
+                })
+        }).then(() => {
+            setProcessing(false);
+        });
+    }
+
+    return {
+        input, setInput,
+        messages,
+        submitMessage,
+        processing,
+        ready: chain !== null,
+        progressMessage
+    }
 }
