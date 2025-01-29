fb0b30fda5a3cdca37b4b36e1cc7280cf066b7db
Miguel Escriva <miesgre@gmail.com>
2025-01-29 13:09:01 +0100

    Rename the package and update the webpack configuration for the new AI plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const config = require('./webpack.common');
 config.entry = './src/index.js',
 config.output = {
 	path: path.join(__dirname, "dist"),
-	filename: 'paella-webgl-plugins.js',
-	library: 'paella-webgl-plugins',
+	filename: 'paella-ai-plugins.js',
+	library: 'paella-ai-plugins',
 	libraryTarget: 'umd'
 };
 config.externals = {
